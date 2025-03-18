41567d0720295ed33b32ff2054e29fd2e07abdf0
imankamrann <imankamran@gmail.com>
2025-03-18 11:20:22 -0400

    Initial Commit

---
+++ WebSocketClientApp/WebSocketClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketClientAppApp.swift
+//  WebSocketClientApp
+//
+//  Created by Iman on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
